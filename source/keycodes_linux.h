94c84e22f5d355d055729801dfa199607b792309
Nelson <silvnelson@gmail.com>
2025-09-30 22:17:23 +0100

    Added keycodes and a basic windowing

---
+++ source/keycodes_linux.h
@@ -0,0 +1,77 @@
+#ifndef __KEYCODES_WAYLAND_H__
+#define __KEYCODES_WAYLAND_H__
+
+#include <X11/keysym.h>
+
+#define KC_TAB              XK_Tab
+#define KC_RETURN           XK_Return
+#define KC_SHIFT            XK_Shift_L
+#define KC_CONTROL          XK_Control_L
+#define KC_MENU             XK_Menu
+#define KC_ESCAPE           XK_Escape
+#define KC_SPACE            XK_space
+#define KC_PAGE_UP          XK_Page_Up
+#define KC_PAGE_DOWN        XK_Page_Down
+#define KC_END              XK_End
+#define KC_HOME             XK_Home
+#define KC_LEFT             XK_Left
+#define KC_UP               XK_Up
+#define KC_RIGHT            XK_Right
+#define KC_DOWN             XK_Down
+#define KC_INSERT           XK_Insert
+#define KC_DELETE           XK_Delete
+#define KC_0                XK_0
+#define KC_1                XK_1
+#define KC_2                XK_2
+#define KC_3                XK_3
+#define KC_4                XK_4
+#define KC_5                XK_5
+#define KC_6                XK_6
+#define KC_7                XK_7
+#define KC_8                XK_8
+#define KC_9                XK_9
+#define KC_A                XK_A
+#define KC_B                XK_B
+#define KC_C                XK_C
+#define KC_D                XK_D
+#define KC_E                XK_E
+#define KC_F                XK_F
+#define KC_G                XK_G
+#define KC_H                XK_H
+#define KC_I                XK_I
+#define KC_J                XK_J
+#define KC_K                XK_K
+#define KC_L                XK_L
+#define KC_M                XK_M
+#define KC_N                XK_N
+#define KC_O                XK_O
+#define KC_P                XK_P
+#define KC_Q                XK_Q
+#define KC_R                XK_R
+#define KC_S                XK_S
+#define KC_T                XK_T
+#define KC_U                XK_U
+#define KC_V                XK_V
+#define KC_W                XK_W
+#define KC_X                XK_X
+#define KC_Y                XK_Y
+#define KC_Z                XK_Z
+#define KC_MULTIPLY         XK_KP_Multiply
+#define KC_ADD              XK_KP_Add
+#define KC_SEPARATOR        XK_KP_Separator
+#define KC_SUBTRACT         XK_KP_Subtract
+#define KC_DIVIDE           XK_KP_Divide
+#define KC_F1               XK_F1
+#define KC_F2               XK_F2
+#define KC_F3               XK_F3
+#define KC_F4               XK_F4
+#define KC_F5               XK_F5
+#define KC_F6               XK_F6
+#define KC_F7               XK_F7
+#define KC_F8               XK_F8
+#define KC_F9               XK_F9
+#define KC_F10              XK_F10
+#define KC_F11              XK_F11
+#define KC_F12              XK_F12
+
+#endif
